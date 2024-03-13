2a70627f31e7ec48e9b346d39adb48fdf0f80959
KaffeineKid <vanessa.m.a1989@gmail.com>
2024-03-13 18:09:03 +0100

    --no-edit

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Hello world!</h1>
+      <h1>Hello world</h1>
     </main>
   );
 }
