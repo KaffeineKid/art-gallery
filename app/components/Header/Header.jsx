98628d6256cae4ca5179198984a27d80cd452bff
KaffeineKid <vanessa.m.a1989@gmail.com>
2024-03-14 21:33:52 +0100

    created header component and rendered it in app layout

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+import styles from "./header.module.css";
+
+export default function () {
+  return <h1 className={styles.header}> The Art Gallery </h1>;
+}
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,18 @@
 import "./globals.css";
 import { Truculenta } from "next/font/google";
+import { Kanit } from "next/font/google";
+import Header from "./components/Header/Header";
 
-const truculenta = Truculenta({ subsets: ["latin"], weight: ["100"]});
+const truculenta = Truculenta({
+  subsets: ["latin"],
+  weight: ["100"],
+  variable: "--font-truculenta",
+});
+const kanit = Kanit({
+  subsets: ["latin"],
+  weight: ["700"],
+  variable: "--font-kanit",
+});
 
 export const metadata = {
   title: "The Art Gallery",
@@ -11,8 +22,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={truculenta.className}>
-      {children}
+      <body className={`${kanit.variable} ${truculenta.variable}`}>
+        {children}
+        <Header />
       </body>
     </html>
   );
